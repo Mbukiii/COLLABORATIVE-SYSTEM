952be2dc950a768a068e575eb24de334d86f7985
Mark-kizz <markkathurima0005@gmail.com>
2025-05-01 11:13:33 +0300

    Update projectController.js

--- backend/controllers/projectController.js
+++ backend/controllers/projectController.js
@@ -1,1 +1,30 @@
+const Project = require('../models/Project');
+const Task = require('../models/Task');
 
+// Get all projects for current user
+exports.getProjects = async (req, res) => {
+  try {
+    const projects = await Project.find({ user: req.user.userId })
+      .sort({ createdAt: -1 });
+    res.json(projects);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
+
+// Get project by ID
+exports.getProjectById = async (req, res) => {
+  try {
+    const project = await Project.findOne({
+      _id: req.params.id,
+      user: req.user.userId
+    });
+    
+    if (!project) {
+      return res.status(404).json({ message: 'Project not found' });
+    }
+    res.json(project);
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+};
